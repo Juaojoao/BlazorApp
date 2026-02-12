e65acc5a1c4e8c0a0e7f64cd3d9a19258c814fb1
João <soloqf@gmail.com>
2026-02-12 13:48:18 -0300

    first commit

---
+++ Data/MovieContext.cs
@@ -0,0 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+using BlazorWebAppMovies.Models;
+
+namespace BlazorWebAppMovies.Data;
+
+public class MovieContext : DbContext
+{
+    public MovieContext(DbContextOptions<MovieContext> options) : base(options) { }
+    public DbSet<Movie> Movie { get; set; } = default!;
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+using BlazorWebAppMovies.Components;
+using Microsoft.EntityFrameworkCore;
+using BlazorWebAppMovies.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+var connectionString = builder.Configuration.GetConnectionString("MovieContext");
+
+builder.Services.AddDbContextFactory<MovieContext>(options =>
+    options.UseNpgsql(connectionString));
+
+builder.Services.AddQuickGridEntityFrameworkAdapter();
+
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+    app.UseMigrationsEndPoint();
+}
+
+app.UseStatusCodePagesWithReExecute("/not-found");
+
+app.UseHttpsRedirection();
+
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
